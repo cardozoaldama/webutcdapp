ac4fd100e31c59a3de403d79b01523422b4eb3f8
Fernando Cardozo <nuevo_10000@outlook.com>
2023-05-20 20:35:55 -0400

    Create table departamento with official data

---
+++ queries/insert_departamento.sql
@@ -0,0 +1,22 @@
+INSERT INTO public.sys_departamento (
+    id_departamento, nombre, observacion, activo, codigo, id_cuenta
+) VALUES 
+    (103, 'concepcion', 'N/A', 'Sí', 'a', 3),
+    (104, 'san pedro', 'N/A', 'Sí', 'b', 4),
+    (105, 'cordillera', 'N/A', 'Sí', 'c', 2),
+    (106, 'guaira', 'N/A', 'Sí', 'd', 1),
+    (107, 'caaguazu', 'N/A', 'Sí', 'e', 5),
+    (108, 'caazapa', 'N/A', 'Sí', 'f', 2),
+    (109, 'itapua', 'N/A', 'Sí', 'g', 4),
+    (110, 'misiones', 'N/A', 'Sí', 'h', 3),
+    (111, 'paraguarí', 'N/A', 'Sí', 'i', 5),
+    (113, 'central', 'N/A', 'Sí', 'j', 1),
+    (114, 'ñeembucu', 'N/A', 'Sí', 'k', 2),
+    (115, 'amambay', 'N/A', 'Sí', 'l', 3),
+    (116, 'canindeyu', 'N/A', 'Sí', 'm', 4),
+    (117, 'presidente hayes', 'N/A', 'Sí', 'n', 5),	
+    (118, 'alto paraguay', 'N/A', 'Sí', 'o', 1),
+    (119, 'boquerón', 'N/A', 'Sí', 'p', 2),
+    (112, 'alto parana', 'N/A', 'Sí', 'q', 3),
+    (120, 'asuncion', 'N/A', 'Sí', 'r', 4),
+    (121, 'asunción', 'N/A', 'Sí', 's', 5);
