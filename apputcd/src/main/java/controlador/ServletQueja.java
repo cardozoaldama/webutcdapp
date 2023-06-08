80465cbbc0f7a926cf572b3a1d41022422b638be
Fernando Cardozo <nuevo_10000@outlook.com>
2023-06-08 09:20:03 -0400

    Added some basic imports

--- apputcd/src/main/java/controlador/ServletQueja.java
+++ apputcd/src/main/java/controlador/ServletQueja.java
@@ -1,7 +1,21 @@
 package controlador;
 
 // IMPORTS
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
-public class ServletQueja {
-	
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.QuejaDAO;
+import entidad.Queja;
+
+public class ServletQueja extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	QuejaDAO quejaDAO;
 }
