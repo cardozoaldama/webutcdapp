b8c5d36a74cf463efd6f71066a4b2b2a1abddee5
Fernando Cardozo <nuevo_10000@outlook.com>
2023-06-08 08:16:21 -0400

    Updated create_reclamo.sql for new issue

--- queries/create_reclamo.sql
+++ queries/create_reclamo.sql
@@ -1,26 +1,37 @@
--- Table: public.sys_departamento
+-- Create sys_reclamo for quejas.jsp
 
--- DROP TABLE IF EXISTS public.sys_departamento;
+CREATE TABLE public.sys_reclamo (
+    id_reclamo integer NOT NULL,
+    numero_reclamo character(45),
+    nis integer,
+    telefono character(20),
+    nombre character(50),
+    apellido character(50),
+    direccion character(150),
+    referencia character(150),
+    numero_movil character(20),
+    correo character(100),
+    observacion character(80),
+    fecha_hora_recepcion timestamp without time zone,
+    asignacion_usuario character(80),
+    id_departamento integer,
+    id_ciudad integer,
+    id_barrio integer,
+    id_cuenta integer
+);
 
-CREATE TABLE IF NOT EXISTS public.sys_departamento
-(
-    id_departamento integer NOT NULL,
-    nombre character(45) COLLATE pg_catalog."default",
-    observacion character(80) COLLATE pg_catalog."default",
-    activo character(40) COLLATE pg_catalog."default",
-    codigo character(25) COLLATE pg_catalog."default",
-    id_cuenta integer,
-    CONSTRAINT sys_departamento_pkey PRIMARY KEY (id_departamento),
-    CONSTRAINT id_cuenta FOREIGN KEY (id_cuenta)
-        REFERENCES public.kwf_cuenta (id_cuenta) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-)
 
-TABLESPACE pg_default;
+ALTER TABLE ONLY public.sys_reclamo
+    ADD CONSTRAINT sys_reclamo_pkey PRIMARY KEY (id_reclamo);
 
-ALTER TABLE IF EXISTS public.sys_departamento
-    OWNER to fcardozo;
+ALTER TABLE ONLY public.sys_reclamo
+    ADD CONSTRAINT id_barrio FOREIGN KEY (id_barrio) REFERENCES public.sys_barrio(id_barrio);
 
-COMMENT ON CONSTRAINT id_cuenta ON public.sys_departamento
-    IS 'Foreign key from kwf_cuenta table';
+ALTER TABLE ONLY public.sys_reclamo
+    ADD CONSTRAINT id_ciudad FOREIGN KEY (id_ciudad) REFERENCES public.sys_ciudad(id_ciudad);
+
+ALTER TABLE ONLY public.sys_reclamo
+    ADD CONSTRAINT id_cuenta FOREIGN KEY (id_cuenta) REFERENCES public.kwf_cuenta(id_cuenta);
+
+ALTER TABLE ONLY public.sys_reclamo
+    ADD CONSTRAINT id_departamento FOREIGN KEY (id_departamento) REFERENCES public.sys_departamento(id_departamento);
