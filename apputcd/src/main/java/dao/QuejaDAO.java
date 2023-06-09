ea1bae15549512bc1d3d7da6378f1e545a11f08a
Fernando Cardozo <nuevo_10000@outlook.com>
2023-06-09 11:34:34 -0400

    Added new method

--- apputcd/src/main/java/dao/QuejaDAO.java
+++ apputcd/src/main/java/dao/QuejaDAO.java
@@ -1,9 +1,19 @@
 package dao;
 
+import java.util.List;
+
+import entidad.Queja;
+
 public class QuejaDAO {
 
 	public QuejaDAO(String jbdcURL, String jbdcUsername, String jbdcPassword) {
 		// TODO Auto-generated constructor stub
 	}
+
+	public List<Queja> listarQuejas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 
 }
