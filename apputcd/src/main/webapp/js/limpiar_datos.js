4ee18b9a966ed4499d1113954f38a606958796f3
Fernando Cardozo <nuevo_10000@outlook.com>
2023-06-10 14:45:14 -0400

    Added new file script limpiar_datos.js

---
+++ apputcd/src/main/webapp/js/limpiar_datos.js
@@ -0,0 +1,12 @@
+/**
+ * LIMPIA DATOS DE UN FORMULARIO
+ */
+
+function limpiarCampos() {
+	document.getElementById("codigo").value = "";
+    document.getElementById("nombre").value = "";
+    document.getElementById("descripcion").value = "";
+    document.getElementById("cantidad").value = "";
+    document.getElementById("precio").value = "";
+    // Resto de los campos de texto
+}
