f23beff97d1090fbf10b18bdfdad542a2970e37e
Fernando Cardozo <nuevo_10000@outlook.com>
2023-06-15 14:40:34 -0400

    Fixed some spaces

--- apputcd/src/main/java/modelo/ModeloQuejas.java
+++ apputcd/src/main/java/modelo/ModeloQuejas.java
@@ -15,8 +15,9 @@ public class ModeloQuejas {
 	private String correo;
 	private String observacion;
 	private static final AtomicLong contador = new AtomicLong(100);
-	
-	public ModeloQuejas(int id_reclamo, String numero_reclamo, String telefono, String nombre, String apellido, String direccion, String referencia, String numero_movil, String correo, String observacion ){
+
+	public ModeloQuejas(int id_reclamo, String numero_reclamo, String telefono, String nombre, String apellido,
+			String direccion, String referencia, String numero_movil, String correo, String observacion) {
 		this.id_reclamo = id_reclamo;
 		this.numero_reclamo = numero_reclamo;
 		// this.nis = nis;
@@ -29,7 +30,9 @@ public class ModeloQuejas {
 		this.correo = correo;
 		this.observacion = observacion;
 	}
-	public ModeloQuejas(String numero_reclamo, String telefono, String nombre, String apellido, String direccion, String referencia, String numero_movil, String correo, String observacion) {
+
+	public ModeloQuejas(String numero_reclamo, String telefono, String nombre, String apellido, String direccion,
+			String referencia, String numero_movil, String correo, String observacion) {
 		this.id_reclamo = (int) contador.incrementAndGet();
 		this.numero_reclamo = numero_reclamo;
 		// this.nis = nis;
@@ -42,80 +45,93 @@ public class ModeloQuejas {
 		this.correo = correo;
 		this.observacion = observacion;
 	}
-	
+
 	// GETTERS
-	
+
 	public int getIdReclamo() {
 		return id_reclamo;
 	}
-	
+
 	public String getNumeroReclamo() {
 		return numero_reclamo;
 	}
+
 	/*
-	public int getNis() {
-		return nis;
-	}
-	*/
+	 * public int getNis() { return nis; }
+	 */
 	public String getTelefono() {
 		return telefono;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public String getApellido() {
 		return apellido;
 	}
+
 	public String getDireccion() {
 		return direccion;
 	}
+
 	public String getNumeroMovil() {
 		return numero_movil;
 	}
+
 	public String getReferencia() {
 		return referencia;
 	}
+
 	public String getCorreo() {
 		return correo;
 	}
+
 	public String getObservacion() {
 		return observacion;
 	}
-	
+
 	// SETTERS
-	
+
 	public void setIdReclamo(int id_reclamo) {
 		this.id_reclamo = id_reclamo;
 	}
+
 	public void setNumeroReclamo(String numero_reclamo) {
 		this.numero_reclamo = numero_reclamo;
 	}
+
 	/*
-	public void setNis(int nis) {
-		this.nis = nis;
-	}
-	*/
+	 * public void setNis(int nis) { this.nis = nis; }
+	 */
 	public void setTelefono(String telefono) {
 		this.telefono = telefono;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
+
 	public void setDireccion(String direccion) {
 		this.direccion = direccion;
 	}
+
 	public void setReferencia(String referencia) {
 		this.referencia = referencia;
 	}
+
 	public void setNumeroMovil(String numero_movil) {
 		this.numero_movil = numero_movil;
 	}
+
 	public void setCorreo(String correo) {
 		this.correo = correo;
 	}
+
 	public void setObservacion(String observacion) {
 		this.observacion = observacion;
 	}
