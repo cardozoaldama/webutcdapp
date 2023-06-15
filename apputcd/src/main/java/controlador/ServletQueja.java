9c929ba06b84d352e0566e723704e1b71469df1a
Fernando Cardozo <nuevo_10000@outlook.com>
2023-06-15 14:40:59 -0400

    Fixed some spaces and replacing id_reclamo for id

--- apputcd/src/main/java/controlador/ServletQueja.java
+++ apputcd/src/main/java/controlador/ServletQueja.java
@@ -20,29 +20,33 @@ import entidad.Queja;
 public class ServletQueja extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	QuejaDAO quejaDAO;
-	
+
+	@Override
 	public void init() {
 		String jbdcURL = getServletContext().getInitParameter("URL");
 		String jbdcUsername = getServletContext().getInitParameter("USUARIO");
 		String jbdcPassword = getServletContext().getInitParameter("CLAVE");
-		
+
 		try {
 			quejaDAO = new QuejaDAO(jbdcURL, jbdcUsername, jbdcPassword);
 		} catch (Exception e) {
 			// TODO: Handle exception
 		}
 	}
+
 	public ServletQueja() {
 		super();
 		// TODO: Not Auto-generated constructor stub
 	}
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		System.out.println("Hello Servlet into ServletQueja.java!");
 		String action = request.getParameter("action");
 		System.out.println(action);
 		try {
-			switch(action) {
+			switch (action) {
 			case "index":
 				index(request, response);
 				break;
@@ -54,7 +58,7 @@ public class ServletQueja extends HttpServlet {
 				registrar(request, response);
 				break;
 			case "mostrar":
-				mostrar(request,response);
+				mostrar(request, response);
 				break;
 			case "showedit":
 				showEditar(request, response);
@@ -84,97 +88,166 @@ public class ServletQueja extends HttpServlet {
 			e.getStackTrace();
 		}
 	}
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)	throws ServletException, IOException {
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		System.out.println("Hola ServletQueja.java ...");
 		doGet(request, response);
 	}
-	private void index(HttpServletRequest request, HttpServletResponse response) throws SQLException, ServletException, IOException {
+
+	private void index(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException, ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("principal.jsp");
 		dispatcher.forward(request, response);
 	}
-	private void registrar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
-		// Queja queja = new Queja(0, request.getParameter("numero_reclamo"), Integer.parseInt(request.getParameter("nis")), request.getParameter("telefono"), request.getParameter("nombre"), request.getParameter("apellido"), request.getParameter("direccion"), request.getParameter("referencia"), request.getParameter("numero_movil"), request.getParameter("correo"), request.getParameter("observacion"), request.getParameter("departamento"), request.getParameter("ciudad"), request.getParameter("barrio"));
-		// Queja queja = new Queja(0, Integer.parseInt(request.getParameter("nis")), request.getParameter("telefono"), request.getParameter("nombre"), request.getParameter("apellido"), request.getParameter("direccion"), request.getParameter("referencia"), request.getParameter("correo"), request.getParameter("departamento"), request.getParameter("ciudad"), request.getParameter("barrio"));
-		Queja queja = new Queja(0, request.getParameter("nombre"), request.getParameter("apellido"), request.getParameter("telefono"), request.getParameter("departamento"), request.getParameter("ciudad"), request.getParameter("barrio"), request.getParameter("direccion"), request.getParameter("correo"), request.getParameter("referencia"));
+
+	private void registrar(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException, SQLException {
+		// Queja queja = new Queja(0, request.getParameter("numero_reclamo"),
+		// Integer.parseInt(request.getParameter("nis")),
+		// request.getParameter("telefono"), request.getParameter("nombre"),
+		// request.getParameter("apellido"), request.getParameter("direccion"),
+		// request.getParameter("referencia"), request.getParameter("numero_movil"),
+		// request.getParameter("correo"), request.getParameter("observacion"),
+		// request.getParameter("departamento"), request.getParameter("ciudad"),
+		// request.getParameter("barrio"));
+		// Queja queja = new Queja(0, Integer.parseInt(request.getParameter("nis")),
+		// request.getParameter("telefono"), request.getParameter("nombre"),
+		// request.getParameter("apellido"), request.getParameter("direccion"),
+		// request.getParameter("referencia"), request.getParameter("correo"),
+		// request.getParameter("departamento"), request.getParameter("ciudad"),
+		// request.getParameter("barrio"));
+		Queja queja = new Queja(0, request.getParameter("nombre"), request.getParameter("apellido"),
+				request.getParameter("telefono"), request.getParameter("departamento"), request.getParameter("ciudad"),
+				request.getParameter("barrio"), request.getParameter("direccion"), request.getParameter("correo"),
+				request.getParameter("referencia"));
 		quejaDAO.insertar(queja, request);
 		request.setAttribute("mensaje", "Los datos se insertaron correctamente en el ServletQueja.java");
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/vista/quejas.jsp");
 		dispatcher.forward(request, response);
 	}
-	
-	private void nuevo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
+
+	private void nuevo(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException, SQLException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/vista/register.jsp");
 		dispatcher.forward(request, response);
 	}
-	
-	private void mostrar(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException , ServletException{
-		RequestDispatcher dispatcher = request.getRequestDispatcher("./vista/mostrar.jsp");
+
+	private void mostrar(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException, IOException, ServletException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/vista/mostrar.jsp");
 		List<Queja> listaQuejas = quejaDAO.listarQuejas();
 		request.setAttribute("lista", listaQuejas);
 		dispatcher.forward(request, response);
 	}
-	
-	private void showEditar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
-		Queja queja = quejaDAO.obtenerPorId(Integer.parseInt(request.getParameter("id_reclamo")));
+
+	private void showEditar(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException, SQLException {
+		Queja queja = quejaDAO.obtenerPorId(Integer.parseInt(request.getParameter("id")));
 		request.setAttribute("queja", queja);
 		RequestDispatcher dispatcher = request.getRequestDispatcher("./vista/editarQuejas.jsp");
 		dispatcher.forward(request, response);
 	}
-	private void editar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException  {
-		// Queja queja = new Queja(request.getParameter("id_reclamo"), request.getParameter("numero_reclamo"), Integer.parseInt(request.getParameter("nis")), request.getParameter("telefono"), request.getParameter("nombre"), request.getParameter("apellido"), request.getParameter("direccion"), request.getParameter("referencia"), request.getParameter("numero_movil"), request.getParameter("correo"), request.getParameter("observacion"), request.getParameter("departamento"), request.getParameter("ciudad"), request.getParameter("barrio"));
-		// Queja queja = new Queja(Integer.parseInt(request.getParameter("id_reclamo")), Integer.parseInt(request.getParameter("nis")), request.getParameter("telefono"), request.getParameter("nombre"), request.getParameter("apellido"), request.getParameter("direccion"), request.getParameter("referencia"), request.getParameter("correo"), request.getParameter("departamento"), request.getParameter("ciudad"), request.getParameter("barrio"));
-		Queja queja = new Queja(Integer.parseInt(request.getParameter("id_reclamo")), request.getParameter("nombre"), request.getParameter("apellido"), request.getParameter("telefono"), request.getParameter("departamento"), request.getParameter("ciudad"), request.getParameter("barrio"), request.getParameter("direccion"), request.getParameter("correo"), request.getParameter("referencia"));
+
+	private void editar(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException, SQLException {
+		Queja queja = new Queja(Integer.parseInt(request.getParameter("id")), request.getParameter("nombre"),
+				request.getParameter("apellido"), request.getParameter("telefono"),
+				request.getParameter("departamento"), request.getParameter("ciudad"), request.getParameter("barrio"),
+				request.getParameter("direccion"), request.getParameter("correo"), request.getParameter("referencia"));
 		quejaDAO.actualizar(queja);
 		index(request, response);
 	}
-	private void eliminar(HttpServletRequest request, HttpServletResponse response) throws SQLException, ServletException, IOException {
-		Queja queja = quejaDAO.obtenerPorId(Integer.parseInt(request.getParameter("id_reclamo")));
+
+	private void eliminar(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException, ServletException, IOException {
+		Queja queja = quejaDAO.obtenerPorId(Integer.parseInt(request.getParameter("id")));
 		quejaDAO.actualizarInactivar(queja, request);
 		RequestDispatcher dispatcher = request.getRequestDispatcher("principal.jsp");
-		dispatcher.forward(request, response);		
-	}
-	
-	private void ejemplo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/vista/example.jsp");
-		List<Queja> listaQuejas= quejaDAO.listarQuejas();
-		request.setAttribute("lista", listaQuejas);
 		dispatcher.forward(request, response);
 	}
-	
-	private void mostrarQuejas(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
-		RequestDispatcher dispatcher = request.getRequestDispatcher("./vista/mostrarQuejas.jsp");
+
+	/*
+	 * 
+	 * private void showEditar(HttpServletRequest request, HttpServletResponse
+	 * response) throws ServletException, IOException, SQLException { Queja queja =
+	 * quejaDAO.obtenerPorId(Integer.parseInt(request.getParameter("id")));
+	 * request.setAttribute("queja", queja); RequestDispatcher dispatcher =
+	 * request.getRequestDispatcher("/vista/editarQuejas.jsp");
+	 * dispatcher.forward(request, response); } private void
+	 * editar(HttpServletRequest request, HttpServletResponse response) throws
+	 * ServletException, IOException, SQLException { // Queja queja = new
+	 * Queja(request.getParameter("id_reclamo"),
+	 * request.getParameter("numero_reclamo"),
+	 * Integer.parseInt(request.getParameter("nis")),
+	 * request.getParameter("telefono"), request.getParameter("nombre"),
+	 * request.getParameter("apellido"), request.getParameter("direccion"),
+	 * request.getParameter("referencia"), request.getParameter("numero_movil"),
+	 * request.getParameter("correo"), request.getParameter("observacion"),
+	 * request.getParameter("departamento"), request.getParameter("ciudad"),
+	 * request.getParameter("barrio")); // Queja queja = new
+	 * Queja(Integer.parseInt(request.getParameter("id_reclamo")),
+	 * Integer.parseInt(request.getParameter("nis")),
+	 * request.getParameter("telefono"), request.getParameter("nombre"),
+	 * request.getParameter("apellido"), request.getParameter("direccion"),
+	 * request.getParameter("referencia"), request.getParameter("correo"),
+	 * request.getParameter("departamento"), request.getParameter("ciudad"),
+	 * request.getParameter("barrio")); Queja queja = new
+	 * Queja(Integer.parseInt(request.getParameter("id")),
+	 * request.getParameter("nombre"), request.getParameter("apellido"),
+	 * request.getParameter("telefono"), request.getParameter("departamento"),
+	 * request.getParameter("ciudad"), request.getParameter("barrio"),
+	 * request.getParameter("direccion"), request.getParameter("correo"),
+	 * request.getParameter("referencia")); quejaDAO.actualizar(queja);
+	 * index(request, response); } private void eliminar(HttpServletRequest request,
+	 * HttpServletResponse response) throws SQLException, ServletException,
+	 * IOException { Queja queja =
+	 * quejaDAO.obtenerPorId(Integer.parseInt(request.getParameter("id")));
+	 * quejaDAO.actualizarInactivar(queja, request); RequestDispatcher dispatcher =
+	 * request.getRequestDispatcher("principal.jsp"); dispatcher.forward(request,
+	 * response); }
+	 * 
+	 */
+	private void ejemplo(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException, SQLException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/vista/example.jsp");
 		List<Queja> listaQuejas = quejaDAO.listarQuejas();
 		request.setAttribute("lista", listaQuejas);
 		dispatcher.forward(request, response);
 	}
-	
+
+	private void mostrarQuejas(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException, SQLException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/vista/mostrarQuejas.jsp");
+		List<Queja> listaQuejas = quejaDAO.listarQuejas();
+		request.setAttribute("lista", listaQuejas);
+		dispatcher.forward(request, response);
+		System.out.println(listaQuejas);
+	}
+
 	// Se ha cambiado totalmente del original ServletArticulo.java
-	private void registrarQuejas(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
-		RequestDispatcher dispatcher = request.getRequestDispatcher("./vista/quejas.jsp");
+	private void registrarQuejas(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException, SQLException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/vista/quejas.jsp");
 		dispatcher.forward(request, response);
 	}
+
 	private void comboDepartamento(HttpServletRequest request, HttpServletResponse response) {
 		// TODO Auto-generated method stub
-		
+
 	}
-		
+
 	/*
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("Hello doPost from ServletQueja.java");
-		String action = request.getParameter("action");
-		
-		if (action != null && action.equals("ComboCiudad")) {
-			try {
-				comboCiudad(request, response);
-			} catch (SQLException | IOException | ServletException e) {
-				e.printStackTrace();
-			}
-		} else {
-			// I don't know, honest...
-		}
-		doGet(request, response);
-	}
-	*/
-	
+	 * protected void doPost(HttpServletRequest request, HttpServletResponse
+	 * response) throws ServletException, IOException {
+	 * System.out.println("Hello doPost from ServletQueja.java"); String action =
+	 * request.getParameter("action");
+	 * 
+	 * if (action != null && action.equals("ComboCiudad")) { try {
+	 * comboCiudad(request, response); } catch (SQLException | IOException |
+	 * ServletException e) { e.printStackTrace(); } } else { // I don't know,
+	 * honest... } doGet(request, response); }
+	 */
+
 }
