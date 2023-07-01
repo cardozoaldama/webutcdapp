6d9b39c4f9fb34282c3903a9e425f2f782bdee4b
Fernando Cardozo <nuevo_10000@outlook.com>
2023-07-01 10:33:42 -0400

    Actualizado el getter y setter de departamento

--- apputcd/src/main/java/entidad/Departamento.java
+++ apputcd/src/main/java/entidad/Departamento.java
@@ -1,26 +1,26 @@
 package entidad;
 
 public class Departamento {
-	private Integer id_departamento;
+	private Integer iddepartamento;
 	private String nombre;
 	private String observacion;
 
-	public Departamento(int id_departamento, String nombre, String observacion) {
-		this.id_departamento = id_departamento;
+	public Departamento(int iddepartamento, String nombre, String observacion) {
+		this.iddepartamento = iddepartamento;
 		this.nombre = nombre;
 		this.observacion = observacion;
 	}
 	// getters y setters
 
 	public Departamento(Integer id) {
-		this.id_departamento = id;
+		this.iddepartamento = id;
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((id_departamento == null) ? 0 : id_departamento.hashCode());
+		result = prime * result + ((iddepartamento == null) ? 0 : iddepartamento.hashCode());
 		return result;
 	}
 
@@ -31,20 +31,20 @@ public class Departamento {
 		if ((obj == null) || (getClass() != obj.getClass()))
 			return false;
 		Departamento other = (Departamento) obj;
-		if (id_departamento == null) {
-			if (other.id_departamento != null)
+		if (iddepartamento == null) {
+			if (other.iddepartamento != null)
 				return false;
-		} else if (!id_departamento.equals(other.id_departamento))
+		} else if (!iddepartamento.equals(other.iddepartamento))
 			return false;
 		return true;
 	}
 
-	public Integer getId_departamento() {
-		return id_departamento;
+	public Integer getIddepartamento() {
+		return iddepartamento;
 	}
 
-	public void setId_departamento(Integer id_departamento) {
-		this.id_departamento = id_departamento;
+	public void setIddepartamento(Integer iddepartamento) {
+		this.iddepartamento = iddepartamento;
 	}
 
 	public String getNombre() {
